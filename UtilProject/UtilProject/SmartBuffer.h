ff05eb79ade17a90d50629ccd9fd9f46d407485f
fbjiang <fbjiang@secusoft.cc>
2018-11-08 20:45:36 +0800

    improve smartbuffer

--- UtilProject/UtilProject/SmartBuffer.h
+++ UtilProject/UtilProject/SmartBuffer.h
@@ -27,15 +27,22 @@ public:
 		return _capacity;
 	}
 
+    //append data to the buffer tail. if buffer capacity is not enough
+    //and if increase is true ,it will realloc the property buffer size
+    //otherwist, return zero
+    //Return if failed, return zero, or return size append
 	std::size_t append(char* data, std::size_t size, bool increase = true){
 		std::size_t tail_size = _capacity - _length - (_data - _buffer);
 		std::size_t left_capacity = _capacity - _length;
-		if (size <= tail_size){
+		if (size <= tail_size){ 
+            //If the buffer tail space is large enough, just copy data to buffer
 			memcpy(_data + _length, data, size);
 			_length += size;
 		}
 		else{
 			if (size <= left_capacity){
+                //If the tail space is not enough, but left capacity is enough,
+                //first memmove the data, than copy data
 				memmove(_buffer, _data, _length);
 				_data = _buffer;
 				memcpy(_data + _length, data, size);
@@ -50,6 +57,7 @@ public:
 					_data = _buffer; _length += size;
 				}
 				else{
+                    //If not auto increase buffer, return zero
 					return 0;
 				}
 			}
@@ -57,13 +65,33 @@ public:
 		return size;
 	}
 
+    std::size_t append(char* data){
+        return append(data, strlen(data));
+    }
+
+    void append_string_end(){
+        char end_of_string[1] = { 0 };
+        append(end_of_string, 1);
+    }
+
+    void set_data_size(std::size_t size){
+        if (_capacity >= size){
+            _data = _buffer;
+            _length = size;
+        }
+        else{
+            reserve(size);
+            _length = size;
+        }
+    }
+
 	//Reset the old data with new data
 	void reset(char* data, int length){
-		reset(); 
-		append(data, length);
+		reset(); append(data, length);
 	}
 
 	//pop data from back of the buffer queue
+    //Return: the size pop from back
 	std::size_t pop_back(std::size_t size){
 		if (size >= _length){
 			reset(); //if size if bigger then actual size, reset the buf
@@ -76,6 +104,7 @@ public:
 	}
 
 	//pop data from front of the buffer queue
+    //Return: the size pop from front
 	std::size_t pop_front(std::size_t size){
 		if (size >= _length){
 			reset(); //if size is bigger then actual size, reset the buf
@@ -98,26 +127,34 @@ public:
 	char* buff(){ return _data; }
 
 	void swap_buf(SmartBuffer& other){
-
+        std::swap(_buffer, other._buffer);
+        std::swap(_data, other._data);
+        std::swap(_capacity, other._capacity);
+        std::swap(_length, other._length);
 	}
 
 	SmartBuffer& operator=(const SmartBuffer& other) = delete;
 	SmartBuffer(SmartBuffer& other) = delete;
 	SmartBuffer(SmartBuffer&& other){
+        _buffer = other._buffer;
+        _data = other._data;
+        _capacity = other._capacity;
+        _length = other._length;
 
+        other._buffer = nullptr;
+        other._data = nullptr;
+        other._capacity = 0;
+        other._length = 0;
 	}
 
 	SmartBuffer& SmartBuffer::operator=(SmartBuffer&& other){
-
+        swap_buf(other);
 	}
 private:
-	SmartBuffer::SmartBuffer(const SmartBuffer& other){}
-	SmartBuffer& SmartBuffer::operator=(const SmartBuffer& other) = delete;
-private:
-	char*		_buffer{ nullptr }; //point to begin of the buffer
-	char*		_data{ nullptr }; //point to begin of data
-	std::size_t			_capacity{ 0 }; //the buffer capacity
-	std::size_t			_length{ 0 }; //actual data length
+	char*		    _buffer{ nullptr }; //point to begin of the buffer
+	char*		    _data{ nullptr }; //point to begin of data
+	std::size_t	    _capacity{ 0 }; //the buffer capacity
+	std::size_t		_length{ 0 }; //actual data length
 };
 
 #endif /*_SMART_BUFFER_H_*/
